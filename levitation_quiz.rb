c71ddcc6242d599fa3a63534bb65662a502a8a3f
Mahmoud Agamy <agamyv@gmail.com>
2018-11-15 08:10:15 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
 	puts "What is the spell that enacts levitation?"
 	ans = gets
-	puts "You passed the quiz!" ? 'Wingardium Leviosa' : levitation_quiz
+	puts "You passed the quiz!" ? ans == 'Wingardium Leviosa' : levitation_quiz
 	
 end
 
